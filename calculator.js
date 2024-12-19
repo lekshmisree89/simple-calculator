f559ed69aaddd4f5c1bd985568fada65e6aff46b
lekshmisree@gmail.com <lekshmisree89@gmail.com>
2024-12-19 11:07:16 -0500

    simple cal

---
+++ calculator.js
@@ -0,0 +1,41 @@
+function calculate() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const operator = document.getElementById('operator').value;
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const resultDiv = document.getElementById('result');
+
+    if (isNaN(num1) || isNaN(num2)) {
+        resultDiv.innerText = 'Please enter valid numbers!';
+        return;
+    }
+
+    let result;
+
+    switch (operator) {
+        case '':
+            result = null
+            break;
+
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 === 0) {
+                resultDiv.innerText = 'Division by zero is not allowed!';
+                return;
+            }
+            result = num1 / num2;
+            break;
+        default:
+            resultDiv.innerText = 'Invalid operator!';
+            return;
+    }
+
+    resultDiv.innerText = `Result: ${result}`;
+}
